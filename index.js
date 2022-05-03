3a5a10adc3fb943c4fb8f1126c6f35645a5fe2b6
monirakibul <monirakibul@gmail.com>
2022-05-03 13:16:31 +0600

    Mongo db connected

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ app.use(cors())
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@emajohncluster.xb5sz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    client.connect();
+    const inventoryCollection = client.db('wareHouse').collection('product');
+
+}
+run().catch(console.dir);
+
 app.get('', (req, res) => {
-    res.send('running')
+    res.send('running');
 })
 
 app.listen(port, () => {
-    console.log('listening port')
+    console.log('listening port', port);
 })
