348cc363f8bed88b0a9c83238f074d3925501aa9
monirakibul <monirakibul@gmail.com>
2022-05-04 22:55:37 +0600

    filter by email added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const app = express();
 const port = process.env.PORT || 5000;
 
 
@@ -10,15 +11,47 @@ app.use(cors())
 app.use(express.json());
 
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@emajohncluster.xb5sz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    client.connect();
-    const inventoryCollection = client.db('wareHouse').collection('product');
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('wareHouse').collection('product');
 
+        // get all inventory 
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            products = await cursor.toArray();
+            res.send(products)
+        });
+
+        // add inventory 
+        app.post('/add-item', async (req, res) => {
+            const newItem = req.body;
+            const result = await inventoryCollection.insertOne(newItem);
+            res.send(result);
+        });
+
+        // get inventory by id 
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await inventoryCollection.findOne(query);
+            res.send(service);
+        });
+
+        // delete by id
+        app.delete('/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        });
+
+    }
+    finally { }
 }
 run().catch(console.dir);
 
